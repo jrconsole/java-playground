4b6b2b63f9545922f245b2cb41e17869e912573f
jrconsole <jrconsole@gmail.com>
2020-11-30 09:03:45 -0800

    initial commit

---
+++ src/main/java/com/galvanize/Application.java
@@ -0,0 +1,10 @@
+package com.galvanize;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("You're all going down");
+    }
+
+}
